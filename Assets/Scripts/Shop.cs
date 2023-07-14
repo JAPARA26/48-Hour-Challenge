0f7e0e092a954fb02934a7f97b0fd1361763796f
JAPARA26 <100307988+JAPARA26@users.noreply.github.com>
2023-07-15 02:41:03 +0400

    0.6

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -5,7 +5,30 @@ using UnityEngine;
 public class Shop : MonoBehaviour
 {
     public GameObject shopUI; // Reference to the shop UI canvas
+    [Header("Sprite To Change")]
+    public SpriteRenderer bodyPart;
+    [Header("Sprites to Cycle Through")]
+    public List<Sprite> options = new List<Sprite>();
 
+    private int CurrentOption = 0;
+    public void NextOption()
+    {
+        CurrentOption++;
+        if(CurrentOption >= options.Count)
+        {
+            CurrentOption = 0;
+        }
+        bodyPart.sprite = options[CurrentOption];
+    }
+    public void PreviusOption()
+    {
+        CurrentOption--;
+        if (CurrentOption <= 0)
+        {
+            CurrentOption = options.Count -1;
+        }
+        bodyPart.sprite = options[CurrentOption];
+    }
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
